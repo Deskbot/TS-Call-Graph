a883df7558d82c03ab3345e582e57831d11ed672
Deskbot <Deskbot@users.noreply.github.com>
2019-04-03 23:10:48 +0100

    Add edge elements

--- src/vis/draw.ts
+++ src/vis/draw.ts
@@ -2,11 +2,17 @@ import * as d3 from "d3";
 
 import { data } from "./d3Input";
 
-type Link = {
+type LinkInput = {
     source: string;
     target: string;
     value: number;
 };
+
+type Link = {
+    source: Node;
+    target: Node;
+    value: number;
+} & d3.SimulationLinkDatum<Node>;
 
 type Node = {
     id: string;
@@ -24,26 +30,40 @@ const container = d3.select('#ts-call-graph').append('svg')
     .attr('width', width);
 
 d3.forceSimulation(nodes)
-    .force('charge', d3.forceManyBody().strength(-5))
+    .force('charge', d3.forceManyBody().strength(-50))
     .force('center', d3.forceCenter(width / 2, height / 2))
     .force('no-overlap', d3.forceCollide)
     .force('edges', d3.forceLink<Node, Link>(links)
         .id(node => node.id)
         .distance(20)
-        .strength(1))
+        .strength(30))
     .on('tick', onTick);
 
 function onTick() {
-    const selectedCircles = d3.select('svg')
+    const allNodeTags = d3.select('svg')
         .selectAll<SVGCircleElement, Node>('circle')
         .data(nodes);
 
-    selectedCircles.enter()
+    allNodeTags.enter()
         .append('circle')
         .attr('r', 5)
-        .merge(selectedCircles)
+        .merge(allNodeTags)
         .attr('cx', node => node.x!)
         .attr('cy', node => node.y!);
 
-    selectedCircles.exit().remove();
+    allNodeTags.exit().remove();
+
+    const allEdgeTags = d3.select('svg')
+        .selectAll<SVGLineElement, Link>("line")
+        .data(links);
+
+    allEdgeTags.enter()
+        .append("line")
+        .merge(allEdgeTags)
+        .attr("x1", edge => edge.source.x!)
+        .attr("y1", edge => edge.source.y!)
+        .attr("x2", edge => edge.target.x!)
+        .attr("y2", edge => edge.target.y!);
+
+    allEdgeTags.exit().remove();
 }
